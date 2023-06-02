89408016e1ad4bc380c4db2ae319193f2d4f0384
CheateRYT <cheaterytyt@gmail.com>
2023-06-02 17:34:51 +0300

    login rework

--- login.php
+++ login.php
@@ -4,29 +4,32 @@ require_once('db.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $login = $_POST['login'];
     $password = $_POST['password'];
-    if(!empty($login) || !empty($password)){
-      $stmt = $conn->prepare("SELECT * FROM `users` WHERE login = ?");
-      $stmt->bind_param("s", $login);
-      $stmt->execute();
-      $result = $stmt->get_result();
-  
-      if ($result->num_rows > 0) {
-          $row = $result->fetch_assoc();
-          $storedPassword = $row['password'];
-  
-          if ($password === $storedPassword) {
-              echo "Вы авторизовались";
-          } else {
-              echo "Неверный пароль";
-          }
-      } else {
-          echo "Пользователь не найден";
-      }
-  
-      $stmt->close();
-      $conn->close();
-    }else{
-      echo "Пожалуйста, заполните все поля формы.";
+
+    // Проверка наличия данных в полях формы
+    if (!empty($login) && !empty($password)) {
+        // Подготовленное выражение для запроса
+        $stmt = $conn->prepare("SELECT * FROM `users` WHERE login = ?");
+        $stmt->bind_param("s", $login);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $storedPassword = $row['password'];
+
+            if (password_verify($password, $storedPassword)) {
+                echo "Вы авторизовались";
+            } else {
+                echo "Неверный пароль";
+            }
+        } else {
+            echo "Пользователь не найден";
+        }
+
+        $stmt->close();
+        $conn->close();
+    } else {
+        echo "Пожалуйста, заполните все поля формы.";
     }
-  }
-  ?>
+}
+?>
